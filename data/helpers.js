eb489ef83baa6f81c9cf3aa7829b262699bdac1c
theallegrarr <nurudeen.amedu@gmail.com>
2019-11-07 13:43:20 +0100

    get shopping list implemented

--- data/helpers.js
+++ data/helpers.js
@@ -2,11 +2,17 @@ const db = require('../data/db-config');
 
 module.exports = {
   getRecipes,
-  //getShoppingList
+  getShoppingList
 }
 
 function getRecipes(){
   return db('recipes_book').then(res => {
+    return res;
+  }).catch(err => console.log(err))
+}
+
+function getShoppingList(){
+  return db('ingredients').where({ recipe_id: req.params.id }).then(res => {
     return res;
   }).catch(err => console.log(err))
 }
--- routes/recipe-router.js
+++ routes/recipe-router.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const db = require('../data/db-config');
+const helpers = require('../data/helpers');
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
   try {
-    const recipes= await db('recipes_book');
+    const recipes= await helpers.getRecipes();
 
     res.status(200).json(recipes);
   } catch(err) {
     console.log(err);
     res.status(500).json({ message: 'failed to get recipes'})
+  }
+})
+
+router.get('/:id/shoppinglist', async (req, res) => {
+  try {
+    const list= await helpers.getShoppingList();
+
+    res.status(200).json(list);
+  } catch(err) {
+    console.log(err);
+    res.status(500).json({ message: 'failed to get shopping list'})
   }
 })
 
