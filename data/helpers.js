20c2f8564182df38dac621440637e0aba28fdb95
theallegrarr <nurudeen.amedu@gmail.com>
2019-11-07 13:42:14 +0100

    get recipes moved into helper function

---
+++ data/helpers.js
@@ -0,0 +1,12 @@
+const db = require('../data/db-config');
+
+module.exports = {
+  getRecipes,
+  //getShoppingList
+}
+
+function getRecipes(){
+  return db('recipes_book').then(res => {
+    return res;
+  }).catch(err => console.log(err))
+}
